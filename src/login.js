bab92d4c767aba2cb195f8f3fa0dff3decf85b1a
Seba <sebaq3@gmail.com>
2025-07-01 12:28:11 +0200

    guardar html

--- src/login.js
+++ src/login.js
@@ -47,6 +47,11 @@ async function login() {
       }
       return false;
     });
+
+    //Guardar en archivo la pagina
+    const html = await page.content();
+    require('fs').writeFileSync('/tmp/debug.html', html);
+
     await page.screenshot({ path: '/tmp/error_login3.png', fullPage: true });
     if (!continuarClick) throw new Error('❌ No se encontró el botón "Continuar"');
 
